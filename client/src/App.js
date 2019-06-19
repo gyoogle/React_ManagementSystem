f3cda7d9975fc5c6d4b3b5be0c82c338d5327b7e
kim6394 <kim6394@hanmail.net>
2019-06-19 14:09:19 +0900

    add CustomerAdd

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Customer from './components/Customer';
+import CustomerAdd from './components/CustomerAdd';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
@@ -76,33 +77,35 @@ class App extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {this.state.customers ? 
-              this.state.customers.map(c => {
-                return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
-              }) :
+      <div>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
               <TableRow>
-                <TableCell colSpan="6" align="center">
-                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
-                </TableCell>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
               </TableRow>
-            }
-          </TableBody>
-        </Table>
-      </Paper>
-
+            </TableHead>
+            <TableBody>
+              {this.state.customers ? 
+                this.state.customers.map(c => {
+                  return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+                }) :
+                <TableRow>
+                  <TableCell colSpan="6" align="center">
+                    <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+                  </TableCell>
+                </TableRow>
+              }
+            </TableBody>
+          </Table>
+        </Paper>
+        <CustomerAdd/>
+      </div>
     );
   }
 }
--- server.js
+++ server.js
@@ -1,39 +1,25 @@
 const fs = require('fs');
-
 const express = require('express');
-
 const bodyParser = require('body-parser');
-
 const app = express();
-
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
 const data = fs.readFileSync('./database.json');
-
 const conf = JSON.parse(data);
-
 const mysql = require('mysql');
 
-
+const multer = require('multer');
+const upload = multer({dest: './upload'})
 
 const connection = mysql.createConnection({
-
     host: conf.host,
-
     user: conf.user,
-
     password: conf.password,
-
     port: conf.port,
-
     database: conf.database
-
 });
 
 connection.connect();
@@ -43,77 +29,30 @@ connection.connect();
 app.get('/api/customers', (req, res) => {
 
     connection.query(
-
         'SELECT * FROM CUSTOMER',
-
         (err, rows, fields) => {
-
             res.send(rows);
-
         }
-
     )
 
 });
 
+app.use('/image', express.static('./upload'));
 
+app.post('/api/customers', upload.single('image'), (req, res) => {
+    let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)';
+    let image = '/image/' + req.file.filename;
+    let name = req.body.name;
+    let birthday = req.body.birthday;
+    let gender = req.body.gender;
+    let job = req.body.job;
+    let params = [image, name, birthday, gender, job];
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
-
-
-/*
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const port = process.env.PORT || 5000;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id': 1,
-            'image': 'https://placeimg.com/64/64/1',
-            'name': '홍길동',
-            'birth': '931229',
-            'gender': '남자',
-            'job': '대학생'
-        },
-        {
-            'id': 2,
-            'image': 'https://placeimg.com/64/64/2',
-            'name': '이순신',
-            'birth': '931112',
-            'gender': '남자',
-            'job': '군인'
-        },
-        {
-            'id': 3,
-            'image': 'https://placeimg.com/64/64/3',
-            'name': '손흥민',
-            'birth': '920203',
-            'gender': '남자',
-            'job': '축구선수'
-        },
-        {
-            'id': 4,
-            'image': 'https://placeimg.com/64/64/4',
-            'name': '류현진',
-            'birth': '911112',
-            'gender': '남자',
-            'job': '야구선수'
-        },
-        {
-            'id': 5,
-            'image': 'https://placeimg.com/64/64/5',
-            'name': '백종원',
-            'birth': '731112',
-            'gender': '남자',
-            'job': '요리사'
+    connection.query(sql, params, 
+        (err, rows, fields) => {
+            res.send(rows);
         }
-    ]);
+    )
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
-*/
